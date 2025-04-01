2f34c3fa0e5eeab3c48dddb9f958645b91163af6
amannyugondkar <amannyu48gondkar@gmail.com>
2025-04-01 11:06:20 +0530

    Create User.java
    Implement the User class (Authentication & Data Handling)
    
    Attributes: id, username, password
    
    Methods: Authentication logic (authenticate())

---
+++ User.java
@@ -0,0 +1,9 @@
+// User.java
+public class User {
+    // Constructor
+    public User(int id, String username, String password) {}
+    
+    public int getId() {}
+    public String getUsername() {}
+    public boolean authenticate(String password) {}
+}
