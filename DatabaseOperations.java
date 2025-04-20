f275c9f7530e4326d36e8990b4fcb6112578792b
amannyugondkar <amannyu48gondkar@gmail.com>
2025-04-20 17:50:53 +0530

    Create DatabaseOperations.java

---
+++ DatabaseOperations.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface DatabaseOperations {
+    boolean addTask(Task task);
+    boolean editTask(Task task);
+    boolean deleteTask(int taskId);
+    Task getTaskById(int taskId);
+    List<Task> getAllTasks(User user);
+    List<Task> getTasksByPriority(User user, Task.Priority priority);
+}
